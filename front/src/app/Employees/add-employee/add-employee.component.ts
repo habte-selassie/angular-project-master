56ab8382b913ed4b82d202c4f12764f91c0c870f
Habte Selassie <habtuchelsea1000@gmail.com>
2023-06-09 14:01:48 +0300

    update commit

--- front/src/app/Employees/add-employee/add-employee.component.ts
+++ front/src/app/Employees/add-employee/add-employee.component.ts
@@ -2,6 +2,11 @@ import {FormControl} from '@angular/forms'
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { EmployeeService } from 'src/app/core/employee.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Candidate } from 'src/app/Model/candidate';
+import { CandidatesService } from 'src/app/Services/candidates.service';
+
 // import { Employee } from './employee.interface';
 // import { MatTableDataSource, MatPaginator } from '@angular/material';
 // import { ProjectService } from 'src/app/core/project.service';
@@ -16,7 +21,50 @@ import { EmployeeService } from 'src/app/core/employee.service';
 
 export class AddEmployeeComponent implements OnInit {
 
-  
+  showFiller = false;
+  candidateForm : FormGroup
+  candidate: Candidate = { firstname: '', lastname: '', name: '', age: 0, email: '', skills: '' };
+
+  constructor(private service: CandidatesService,
+     private http: HttpClient,
+     private formBuilder:FormBuilder,
+      private router: Router) {
+
+        this.candidateForm = this.formBuilder.group({
+          age:[null],
+          email:[null]
+        })
+       }
+
+  postCandidate(): void {
+    if (this.validateForm()) {
+      const newCandidate: Candidate = {
+        name: this.candidate.name,
+        age: this.candidate.age,
+        email: this.candidate.email,
+        skills: this.candidate.skills,
+        firstname: this.candidate.firstname,
+        lastname: this.candidate.lastname
+      };
+
+      this.service.createCandidate(newCandidate).subscribe(() => {
+        this.router.navigateByUrl('list-candidate');
+      });
+    } else {
+      alert('Please enter valid data');
+    }
+  }
+
+  private validateForm(): boolean {
+    return (
+      this.candidate.firstname.trim() !== '' &&
+      this.candidate.lastname.trim() !== '' &&
+      this.candidate.name.trim() !== '' &&
+      this.candidate.age > 0 &&
+      this.candidate.email.trim() !== '' &&
+      this.candidate.skills.trim() !== ''
+    );
+  }
     ngOnInit(): void {
         
     }
@@ -150,3 +198,57 @@ export class AddEmployeeComponent implements OnInit {
 
 
 }
+
+
+
+
+
+
+
+// import { HttpClient } from '@angular/common/http';
+// import { Component, OnInit } from '@angular/core';
+// import {Router} from '@angular/router'
+// import { Candidate } from 'src/app/Model/candidate';
+// import { CandidatesService } from 'src/app/Services/candidates.service';
+
+// //import { DepFlags } from '@angular/compiler/src/core';
+
+
+
+// @Component({
+//   selector: 'app-add-candidate',
+//   templateUrl: './add-candidate.component.html',
+//   styleUrls: ['./add-candidate.component.css']
+// })
+// export class AddCandidateComponent {
+
+// candidate:Candidate = {firstname : '',lastname : '', name:'', age:0, email:'',skills:''}
+
+// constructor(private service:CandidatesService ,private http :HttpClient ,private router:Router ){}
+
+
+// postCandidate(firstname :any, lastname:any , name:any,age:any,email:any,skills:any){
+//   if(firstname.invalid | lastname.invalid | name.invalid || age.invalid || email.invalid || skills.invalid) {
+//     alert('Please Enter Valid Data')
+//     window.location.reload()
+//   }
+//   else {
+//     const newCandidate: Candidate = {
+//       name:name.value,
+//       age:age.value,
+//       email:email.value,
+//       skills:skills.value,
+//       firstname:firstname.value,
+//       lastname:lastname.value
+//     }
+//     this.service.createCandidate(newCandidate).subscribe(()=>{
+//       this.router.navigateByUrl('list-candidate')
+//     })
+//   }
+// }
+
+// }
+
+
+
+
